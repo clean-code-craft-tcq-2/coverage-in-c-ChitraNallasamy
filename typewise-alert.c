4b84171f7263b21ddbec580feb5a07a6b675e63b
ChitraNallasamy <91539731+ChitraNallasamy@users.noreply.github.com>
2022-02-24 20:28:28 +0530

    Update typewise-alert.c

--- typewise-alert.c
+++ typewise-alert.c
@@ -1,42 +1,58 @@
-#pragma once
+#include "typewise-alert.h"
+#include <stdio.h>
+#include <string.h>
 
-#define COOLING_TYPES 3
-#define TEMPERATURE_LIMITS 2
-#define LOWER_LIMIT_IN_MAP 0
-#define UPPER_LIMIT_IN_MAP 1
-#define PASSIVE_COOLING_LOWER_LIMIT 0
-#define PASSIVE_COOLING_UPPER_LIMIT 35
-#define HI_ACTIVE_COOLING_LOWER_LIMIT 0
-#define HI_ACTIVE_COOLING_UPPER_LIMIT 45
-#define MED_ACTIVE_COOLING_LOWER_LIMIT 0
-#define MED_ACTIVE_COOLING_UPPER_LIMIT 40
+BreachType inferBreach(double value, double lowerLimit, double upperLimit) {
+  if(value < lowerLimit) {
+    return TOO_LOW;
+  }
+  if(value > upperLimit) {
+    return TOO_HIGH;
+  }
+  return NORMAL;
+}
 
-typedef enum {
-  PASSIVE_COOLING,
-  HI_ACTIVE_COOLING,
-  MED_ACTIVE_COOLING
-} CoolingType;
+BreachType classifyTemperatureBreach(int MapForCoolingTypeVsRange[][TEMPERATURE_LIMITS], CoolingType coolingType, double temperatureInC) {
+  int lowerLimit = MapForCoolingTypeVsRange[coolingType][LOWER_LIMIT_IN_MAP];
+  int upperLimit = MapForCoolingTypeVsRange[coolingType][UPPER_LIMIT_IN_MAP];
 
-typedef enum {
-  NORMAL,
-  TOO_LOW,
-  TOO_HIGH
-} BreachType;
+  return inferBreach(temperatureInC, lowerLimit, upperLimit);
+}
 
-typedef enum {
-  TO_CONTROLLER,
-  TO_EMAIL
-} AlertTarget;
+void prepareAlertTextForController(BreachType breachType, char AlertMessage[10]) {
+  const unsigned short header = 0xfeed;
+  snprintf(AlertMessage,10,"%x : %x\n", header, breachType);
+}
 
-typedef struct {
-  CoolingType coolingType;
-  char brand[48];
-} BatteryCharacter;
+void prepareAlertTextForEmail(BreachType breachType, char AlertMessage[44]) {
+   char recepient[] = "a.b@c.com";
+   char AlertMessagesForEmail[][32] = {"Hi, the temperature is Normal",
+                                       "Hi, the temperature is too low", 
+                                       "Hi, the temperature is too high"};
 
-BreachType inferBreach(double value, double lowerLimit, double upperLimit);
-BreachType classifyTemperatureBreach(int MapForCoolingTypeVsRange[][TEMPERATURE_LIMITS], CoolingType coolingType, double temperatureInC);
-void prepareAlertTextForController(BreachType breachType, char AlertMessage[10]);
-void prepareAlertTextForEmail(BreachType breachType, char AlertMessage[44]);
-void printOnConsole(char *stringToBePrinted);
-BreachType PerformBatteryCheck(BatteryCharacter batteryChar, double temperatureInC, int RangeBasedOnCoolingType[][TEMPERATURE_LIMITS]);
-void Initialize_and_Start_BatteryCheckSystem (AlertTarget currentAlertTarget, BatteryCharacter batteryChar, double temperatureInC);
+   snprintf(AlertMessage,44,"%s : %s",recepient,&(AlertMessagesForEmail[breachType][0]));
+}
+
+void printOnConsole(char *stringToBePrinted) {
+    printf("%s",stringToBePrinted);
+}
+
+BreachType PerformBatteryCheck(BatteryCharacter batteryChar, double temperatureInC, int RangeBasedOnCoolingType[][TEMPERATURE_LIMITS]) {
+    return classifyTemperatureBreach(RangeBasedOnCoolingType, batteryChar.coolingType, temperatureInC);
+}
+
+void Initialize_and_Start_BatteryCheckSystem (AlertTarget currentAlertTarget, BatteryCharacter batteryChar, double temperatureInC) {
+    /* Initialize parameters*/
+    void (*pointerToAlertingOptions[]) (BreachType, char *) = {prepareAlertTextForController,prepareAlertTextForEmail};
+    int RangeBasedOnCoolingType[COOLING_TYPES][TEMPERATURE_LIMITS] = {{PASSIVE_COOLING_LOWER_LIMIT, PASSIVE_COOLING_UPPER_LIMIT}, 
+                                                                      {HI_ACTIVE_COOLING_LOWER_LIMIT, HI_ACTIVE_COOLING_UPPER_LIMIT}, 
+                                                                      {MED_ACTIVE_COOLING_LOWER_LIMIT, MED_ACTIVE_COOLING_UPPER_LIMIT}};
+    
+    /* Start the System */
+    BreachType breachType = PerformBatteryCheck(batteryChar, temperatureInC, RangeBasedOnCoolingType);
+    
+    /* Provide Alert on the requested Target*/
+    char MessageToBeDisplayedOnConsole[44] = "";
+    pointerToAlertingOptions[currentAlertTarget](breachType, MessageToBeDisplayedOnConsole);
+    printOnConsole(MessageToBeDisplayedOnConsole);
+}
